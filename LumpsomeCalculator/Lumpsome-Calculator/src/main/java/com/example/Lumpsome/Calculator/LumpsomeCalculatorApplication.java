bd1f762160d62fe9af6c397d2ccfeebc825852e2
tejasvinigoel <tejasvinigamechanger@gmail.com>
2024-05-31 22:47:21 +0530

    first

---
+++ LumpsomeCalculator/Lumpsome-Calculator/src/main/java/com/example/Lumpsome/Calculator/LumpsomeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Lumpsome.Calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumpsomeCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumpsomeCalculatorApplication.class, args);
+	}
+
+}
---
+++ LumpsomeCalculator/Lumpsome-Calculator/src/test/java/com/example/Lumpsome/Calculator/LumpsomeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Lumpsome.Calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumpsomeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
