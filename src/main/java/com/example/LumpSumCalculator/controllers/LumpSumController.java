69f2f34d4357942e25449b7091f592174b8b62ef
tejasvinigoel <tejasvinigamechanger@gmail.com>
2024-06-01 16:31:19 +0530

    code added

--- src/main/java/com/example/LumpSumCalculator/controllers/LumpSumController.java
+++ src/main/java/com/example/LumpSumCalculator/controllers/LumpSumController.java
@@ -14,28 +14,27 @@ import com.example.LumpSumCalculator.parameters.Parameters;
 public class LumpSumController {
 
     @ModelAttribute("params")
-    public Parameters params(){
+    public Parameters params() {
         return new Parameters();
     }
 
     @GetMapping
-    public String getParameter(){
-        return "LumpsumCalculator";
-    }
-
-    @PostMapping
-    public String returnResult(@ModelAttribute("params") Parameters param, Model model){
-        long principal=param.getPrincipal();
-        double roi=param.getRoi();
-        int time=param.getTime();
-        int frequency=param.getFrequency();
-        double amount=principal*Math.pow((1+roi/frequency),time*frequency);
-        double earnings=amount-principal;
-        model.addAttribute("amount",Math.round(amount));
-        model.addAttribute("params",param);
-        model.addAttribute("earnings",Math.round(earnings));
-        model.addAttribute("principal",principal);
+    public String getParameter() {
         return "LumpSumCalculator";
     }
-    
+
+    @PostMapping("/calculate")
+    public String returnResult(@ModelAttribute("params") Parameters param, Model model) {
+        long principal = param.getPrincipal();
+        double roi = param.getRoi();
+        int time = param.getTime();
+        int frequency = param.getFrequency();
+        double amount = principal * Math.pow((1 + roi / (100 * frequency)), time * frequency);
+        double earnings = amount - principal;
+        model.addAttribute("amount", Math.round(amount));
+        model.addAttribute("params", param);
+        model.addAttribute("earnings", Math.round(earnings));
+        model.addAttribute("principal", principal);
+        return "LumpSumCalculator";
+    }
 }
